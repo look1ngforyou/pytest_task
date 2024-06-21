e3319f3e9fef4dcdd30861afd88a6fe00193de58
look1ngforyou <cherepanovmaxim26@gmail.com>
2024-06-21 16:56:23 +0200

    Fixed issues based on code review feedback

--- test_example_for_code_review.py
+++ test_example_for_code_review.py
@@ -7,51 +7,49 @@ from faker import Faker
 
 
 class TestWebsite:
-    TIMEOUT = 5
+    TIMEOUT = 15
     URL = "https://store.steampowered.com/"
 
     @pytest.fixture(scope="function", autouse=True)
     def setup(self):
         self.driver = Chrome()
-        self.wait = WebDriverWait(self.driver, self.TIMEOUT)
-        yield
+        yield self.driver
         self.driver.quit()
 
-    @pytest.mark.parametrize("url, body_xpath, main_page_xpath, username_xpath, password_xpath, error_xpath",
-                             [(URL, (By.XPATH, '//body'),
-                               (By.XPATH, '//a[contains(@class, "global_action_link")]'),
-                               (By.XPATH, '(//input[contains(@type, "text")])[1]'),
-                               (By.XPATH, '//input[contains(@type, "password")]'),
-                               (By.XPATH, "//div[contains(text(), 'Пожалуйста, проверьте')]"))])
-    def test_combined_function(self, url, body_xpath, main_page_xpath, username_xpath, password_xpath, error_xpath):
+    @pytest.mark.parametrize(
+        "url, body_xpath, main_page_xpath, login_page_xpath, username_xpath, password_xpath, login_xpath, error_xpath",
+        [(URL, (By.XPATH, '//a[contains(@class, "promo_link")]'),
+          (By.XPATH, '//a[contains(@class, "global_action_link")]'),
+          (By.XPATH, '//div[contains(@style, "grid-template-columns: repeat(41, 1fr);")]'),
+          (By.XPATH, '(//input[contains(@type, "text")])[1]'),
+          (By.XPATH, '//input[contains(@type, "password")]'),
+          (By.XPATH, '//button[contains(@type, "submit")]'),
+          (By.XPATH, "//div[contains(text(), 'Пожалуйста, проверьте')]"))])
+    def test_combined_function(self, url, body_xpath, main_page_xpath, login_page_xpath, username_xpath, password_xpath,
+                               login_xpath, error_xpath):
         self.driver.get(url)
+        wait = WebDriverWait(self.driver, timeout=self.TIMEOUT)
         fake = Faker()
-        try:
-            page_displaying = self.wait.until(ec.presence_of_element_located(body_xpath))
-            assert page_displaying is not None
-            print(' The main page is displayed.')
 
-            login_page_button_transmission = self.wait.until(ec.element_to_be_clickable(main_page_xpath))
-            login_page_button_transmission.click()
-            assert self.driver.current_url[
-                   :37] == 'https://store.steampowered.com/login/'  # можно ли такой ассерт юзать?
-            print('The login page is opened.')
+        page_displaying = wait.until(ec.presence_of_element_located(body_xpath))
+        assert page_displaying is not None
+        print('The main page is displayed.')
 
-            username_input = self.wait.until(ec.element_to_be_clickable(username_xpath))
-            password_input = self.wait.until(ec.element_to_be_clickable(password_xpath))
+        login_page_button_transmission = wait.until(ec.element_to_be_clickable(main_page_xpath))
+        login_page_button_transmission.click()
 
-            username_input.send_keys(fake.email())
-            password_input.send_keys(fake.password())
+        assert wait.until(ec.visibility_of_element_located(login_page_xpath))
+        print('The login page is opened.')
 
-            login_button = self.wait.until(ec.element_to_be_clickable(
-                (By.XPATH, '//button[contains(@type, "submit")]')))
-            login_button.click()
-            unsuccessful_login = self.wait.until(ec.presence_of_element_located(error_xpath))
-            assert unsuccessful_login is not None
-            print('The error text is displayed.')
-        except Exception:
-            pytest.fail()
+        username_input = wait.until(ec.element_to_be_clickable(username_xpath))
+        password_input = wait.until(ec.element_to_be_clickable(password_xpath))
 
+        username_input.send_keys(fake.email())
+        password_input.send_keys(fake.password())
 
-if __name__ == "__main__":
-    TestWebsite()
+        login_button = wait.until(ec.element_to_be_clickable(login_xpath))
+        login_button.click()
+
+        unsuccessful_login = wait.until(ec.presence_of_element_located(error_xpath))
+        assert unsuccessful_login is not None
+        print('The error text is displayed.')
