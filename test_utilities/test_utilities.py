ad0fbb53f3883af54389ef9f86a8aa6bc7803135
look1ngforyou <cherepanovmaxim26@gmail.com>
2024-08-01 12:30:51 +0200

    Trying to push test_utilities once again

--- test_utilities/test_utilities.py
+++ test_utilities/test_utilities.py
@@ -3,7 +3,7 @@ import time
 
 
 def upload_file_via_fe(file_path):
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.write(file_path)
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('enter')
